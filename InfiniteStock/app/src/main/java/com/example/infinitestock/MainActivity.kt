9347d4417126b43f82441c7a8785e24fa504764c
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2021-06-02 23:11:18 +0700

    Add MainActivity.kt

---
+++ InfiniteStock/app/src/main/java/com/example/infinitestock/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.infinitestock
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.infinitestock.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
