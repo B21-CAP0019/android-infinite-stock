fb0ae328dbf126b4d4e512374d90fcdbd9bb053f
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2021-06-03 12:59:33 +0700

    Fix Build Failed

--- InfiniteStock/app/src/main/java/com/example/infinitestock/ui/login/LoginActivity.kt
+++ InfiniteStock/app/src/main/java/com/example/infinitestock/ui/login/LoginActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.example.infinitestock.R
 
 class LoginActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
