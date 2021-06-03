4d3fde48722a9f7412422ed719b34262a9f3176d
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2021-06-03 20:55:23 +0700

    put some functions on Sign In

--- InfiniteStock/app/src/main/java/com/example/infinitestock/ui/login/LoginActivity.kt
+++ InfiniteStock/app/src/main/java/com/example/infinitestock/ui/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.infinitestock.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.infinitestock.R
@@ -9,15 +10,19 @@ import com.example.infinitestock.databinding.ActivitySignInBinding
 import com.example.infinitestock.ui.main.MainActivity
 import com.example.infinitestock.ui.signup.SignUpActivity
 import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import cz.msebera.android.httpclient.Header
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignInBinding
-
+    private var _binding: ActivitySignInBinding? = null
+    val binding get() = _binding!!
+    private var isLoading = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
+        _binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Glide.with(this@LoginActivity)
@@ -37,12 +42,41 @@ class LoginActivity : AppCompatActivity() {
         val email = binding.valueSigninEmail.text.toString()
         val password = binding.valueSigninPass.text.toString()
 
-        val endpoint = resources.getString(R.string.server) + "/auth/signin"
+        val url = resources.getString(R.string.server) + "/auth/signin"
         val client = AsyncHttpClient()
 
+        val params = RequestParams()
+        params.put("email", email)
+        params.put("password", password)
 
+        client.post(url, params, object: AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                // success
+            }
 
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                TODO("Not yet implemented")
+            }
+
+        })
         val intentToMainActivity = Intent(this@LoginActivity, MainActivity::class.java)
         startActivity(intentToMainActivity)
+    }
+
+    private fun performLoading() {
+        with (binding) {
+            if (!isLoading) {
+                btnSignin.visibility = View.GONE
+            }
+        }
     }
 }
